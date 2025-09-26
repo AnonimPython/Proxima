7d01a5ad6986b4aa69dce9a987c28a165dc65a90
AnonimPython <moscow.retro@list.ru>
2025-09-26 04:40:58 +0300

    translation of commands into a separate file

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,3 +1,25 @@
 '''
-FILE FOR WORK WHITH STANDART COMMANDS
+FILE FOR WORK WITH STANDART COMMANDS
 '''
+
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, CallbackQuery
+
+router = Router()
+
+# Обработчик команды /start
+@router.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer(
+        "🚀 <b>Добро пожаловать в Proxima!</b>\n\n"
+        "Присоединяйся к элитному сообществу\n"
+        "<i>Для начала пройди регистрацию</i>\n\n"
+        "📋 <b>Доступные команды:</b>\n"
+        "/help - Помощь\n\n"
+        "/register"
+        "🎯 <b>Начни прямо сейчас!</b>"
+    )
+
+
+    
