3f2ce3692b58dcccce69ba1f7dc1cdaa93b727d2
AnonimPython <moscow.retro@list.ru>
2025-09-26 04:45:05 +0300

    create a file for work with keyboards

---
+++ app/handlers/keyboards.py
@@ -0,0 +1,4 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardButton,InlineKeyboardMarkup)
+#* Usings Bilders
+from aiogram.utils.keyboard import ReplyKeyboardBuilder,InlineKeyboardBuilder
